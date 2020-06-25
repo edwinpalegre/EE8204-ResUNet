e6ed69b569baa69aa5e23b7c9fcfa0784cb79078
Edwin Alegre <edwin.p.alegre@ryerson.ca>
2020-06-25 15:49:09 -0400

    Edited patch_sampler.py such that it can create the new sampled data while filtering out patches generated from the occluded original images

--- patch_sampler.py
+++ patch_sampler.py
@@ -12,14 +12,16 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import Image
 
-
+### PARAMETERS ###
 IMAGE_SIZE = 224
 OVERLAP = 14
 NUM_OF_PATCHES = 10
 
+### PATHS ###
 sample_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/samples'
 train_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/training'
 
+# VERIFY IF SAMPLE FOLDER EXISTS
 if os.path.isdir(sample_dataset) == False:
     os.chdir('C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/')
     os.mkdir('samples')
@@ -30,49 +32,56 @@ if os.path.isdir(sample_dataset) == False:
 else:
     sampled_image_path = os.path.join(sample_dataset, 'image').replace('\\','/')
     sampled_mask_path = os.path.join(sample_dataset, 'mask').replace('\\','/')
-    
+
+# IMAGE FILE PARAMETERS
 _, _, train_files = next(os.walk(os.path.join(train_dataset, 'image')))
-training_imgs = len(train_files)   
-new_id_name = 1 
+training_imgs = len(train_files)
 
-#### START LOOP HERE ####
+def get_randompatch():
+    # Randomly choose image from list of all available training images
+    id_name = np.random.randint(1, training_imgs + 1)
+    
+    # Set image and corresponding mask paths
+    image_path = os.path.join(train_dataset, 'image', str(id_name)).replace('\\','/') + '.png'
+    mask_path = os.path.join(train_dataset, 'mask', str(id_name)).replace('\\','/') + '.png'
+    
+    # Read image and map, get shape to help with size restrictions
+    img = Image.open(image_path)
+    mask = Image.open(mask_path)
+    image_shape = img.size
+    mask_shape = mask.size
+    
+    # Display Full Image and Mask
+    # fig = plt.figure(figsize=(20,10))
+    # ax = fig.add_subplot(1, 2, 1)
+    # ax.imshow(img)
+    # ax = fig.add_subplot(1, 2, 2)
+    # ax.imshow(mask, cmap='gray')
+    
+    # Generate randomized crop coordinates
+    start = (np.random.rand(1) * (image_shape[0] - IMAGE_SIZE, image_shape[1] - IMAGE_SIZE)).astype('int')
+    end = start + (IMAGE_SIZE, IMAGE_SIZE)
+    
+    # Crop the actual sampled image and mask from the original, save with new id name
+    cropped_img = img.crop((start[0], start[1], end[1], end[1]))
+    cropped_mask = mask.crop((start[0], start[1], end[1], end[1]))
+    mean_img_val = np.mean(cropped_img)
+    
+    # fig = plt.figure(figsize=(20,10))
+    # ax = fig.add_subplot(1, 2, 1)
+    # ax.imshow(cropped_img)
+    # ax = fig.add_subplot(1, 2, 2)
+    # ax.imshow(cropped_mask, cmap='gray')
 
-id_name = np.random.randint(1, training_imgs + 1)
+    return cropped_img, cropped_mask, mean_img_val, id_name
 
-image_path = os.path.join(train_dataset, 'image', str(id_name)).replace('\\','/') + '.png'
-mask_path = os.path.join(train_dataset, 'mask', str(id_name)).replace('\\','/') + '.png'
+for new_id_name in range(1, 101):
+    threshold_value = 255
+    while(threshold_value > 150):
+        cropped_img, cropped_mask, threshold_value, id_name = get_randompatch()
+    
+    cropped_img.save(os.path.join(sampled_image_path, str(new_id_name) + '-' + str(id_name)) + '.png', 'PNG')
+    cropped_mask.save(os.path.join(sampled_mask_path, str(new_id_name) + '-' + str(id_name)) + '.png', 'PNG')
+    print(new_id_name,'-', id_name, ' = ', threshold_value)
+    new_id_name += 1    
 
-img = Image.open(image_path)
-mask = Image.open(mask_path)
-
-image_shape = img.size
-mask_shape = mask.size
-
-
-# Display Full Image and Mask
-# fig = plt.figure(figsize=(20,10))
-# ax = fig.add_subplot(1, 2, 1)
-# ax.imshow(img)
-# ax = fig.add_subplot(1, 2, 2)
-# ax.imshow(mask, cmap='gray')
-
-# Restrict range of possible values
-max_xdim = image_shape[0] - IMAGE_SIZE
-max_ydim = image_shape[1] - IMAGE_SIZE
-
-patches = []
-
-#for patches in range(NUM_OF_PATCHES):
-start = (np.random.rand(1) * (image_shape[0] - IMAGE_SIZE, image_shape[1] - IMAGE_SIZE)).astype('int')
-end = start + (IMAGE_SIZE, IMAGE_SIZE)
-cropped_img = img.crop((start[0], start[1], end[1], end[1]))
-cropped_mask = mask.crop((start[0], start[1], end[1], end[1]))
-cropped_img.save(os.path.join(sampled_image_path, str(id_name)) + '.png', 'PNG')
-cropped_mask.save(os.path.join(sampled_mask_path, str(id_name)) + '.png', 'PNG')
-
-
-fig = plt.figure(figsize=(20,10))
-ax = fig.add_subplot(1, 2, 1)
-ax.imshow(cropped_img)
-ax = fig.add_subplot(1, 2, 2)
-ax.imshow(cropped_mask, cmap='gray')
