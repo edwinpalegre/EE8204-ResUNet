Working code, gradient accumulation needs to be implemented to make the mini batch size of 8 work properly. Learning rate scheduling also needs to be fixed to reflect the paper

model_resunet.py
@@ -37,7 +37,7 @@ tf.keras.layers.Conv2DTranspose(
 '''
 
 ### LIBRARIES ###
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Add, Conv2DTranspose, concatenate
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, Add, Conv2DTranspose, concatenate, Lambda
 from tensorflow.keras import Model, Input
 from contextlib import redirect_stdout
 import tensorflow as tf
@@ -120,9 +120,10 @@ def ResUNet(inputshape):
     
     # Input
     model_input = Input(shape=inputshape)
+    model_input_float = Lambda(lambda x: x / 255)(model_input)
     
     # Encoder Path
-    model_encoder = encoder(model_input)
+    model_encoder = encoder(model_input_float)
     
     # Bottleneck
     model_bottleneck = res_block(model_encoder[2], 512, [(2, 2), (1, 1)])
@@ -135,7 +136,7 @@ def ResUNet(inputshape):
     
     return Model(model_input, model_output)
     
-model = ResUNet((224, 224, 3))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.summary()
+# model = ResUNet((224, 224, 3))
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# model.summary()
 #tf.keras.utils.plot_model(model, to_file='model.png', show_layer_names=True, show_shapes=True, rankdir='LR')

train.py
@@ -14,27 +14,13 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.optimizers import Adam
-
-## Seeding 
-seed = 2020
-random.seed = seed
-np.random.seed = seed
-tf.seed = seed
+from tensorflow.keras.optimizers import Adam, SGD, schedules
+from tqdm import tqdm
+from skimage.io import imread, imshow
 
 # HYPERPARAMETERS
-
-main_project = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/'
-train_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/training'
-test_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/testing'
-
-_, _, train_files = next(os.walk(os.path.join(train_dataset, 'image')))
-_, _, test_files = next(os.walk(os.path.join(test_dataset, 'image')))
-training_imgs = len(train_files)
-testing_imgs = len(test_files)
-
-train_ids = list(range(1, training_imgs + 1))
-test_ids = list(range(1, testing_imgs + 1))
+seed = 42
+np.random.seed = seed
 
 image_size = 224
 batch_size = 8
@@ -42,7 +28,125 @@ learning_rate = 0.001
 learning_rate_factor = 0.1
 epoch_factor = 20
 overlap = 14
-samples = 30000
+epochs = 50
+
+train_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/samples'
+test_dataset = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/dataset/samples_test'
+
+_, _, train_files = next(os.walk(os.path.join(train_dataset, 'image')))
+_, _, test_files = next(os.walk(os.path.join(test_dataset, 'image')))
+training_imgs = len(train_files)
+test_imgs = len(test_files)
+train_ids = list(range(1, training_imgs + 1))
+test_ids = list(range(1, test_imgs + 1))
+
+X_train = np.zeros((len(train_ids), image_size, image_size, 3), dtype=np.uint8)
+Y_train = np.zeros((len(train_ids), image_size, image_size, 1), dtype=np.bool)
+
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    X_train[n] = imread(train_dataset + '/image/' + str(id_) + '.png')
+    mask = np.zeros((image_size, image_size, 1), dtype=np.bool)
+    for mask_file in next(os.walk(train_dataset  + '/mask/')):
+        mask_ = imread(train_dataset + '/mask/' + str(id_) + '.png')
+        mask_ = np.expand_dims(mask_, axis=-1)
+        mask = np.maximum(mask, mask_)
+    
+    Y_train[n] = mask
+    
+X_test = np.zeros((len(test_ids), image_size, image_size, 3), dtype=np.uint8)
+Y_test = np.zeros((len(test_ids), image_size, image_size, 1), dtype=np.bool)
+sizes_test = []
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    X_test[n] = imread(test_dataset + '/image/' + str(id_) + '.png')
+    mask = np.zeros((image_size, image_size, 1), dtype=np.bool)
+    for mask_file in next(os.walk(test_dataset  + '/mask/')):
+        mask_ = imread(test_dataset + '/mask/' + str(id_) + '.png')
+        mask_ = np.expand_dims(mask_, axis=-1)
+        mask = np.maximum(mask, mask_)
+    
+    Y_test[n] = mask
+
+########################################
+
+'''
+r = random.randint(1, len(train_ids)+1)
+fig = plt.figure()
+fig.subplots_adjust(hspace=1, wspace=1)
+ax = fig.add_subplot(1, 2, 1)
+ax.imshow(X_train[r])
+ax = fig.add_subplot(1, 2, 2)
+ax.imshow(np.reshape(Y_train[r]*255, (image_size, image_size)), cmap="gray")
+'''
+
+# lr_schedule = schedules.PolynomialDecay(initial_learning_rate=0.001, decay_steps=12000, end_learning_rate=0.00001)
+# sgd_optimizer = SGD(learning_rate=lr_schedule)
+sgd_optimizer = SGD(learning_rate=0.001)
+
+model = model_resunet.ResUNet((image_size, image_size, 3))
+model.compile(optimizer=sgd_optimizer, loss='mean_squared_error', metrics=['accuracy'])
+model.summary()
+
+########## BRAND NEW TRAINING ############
+
+checkpoint_path = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/models/resunet.{epoch:02d}-{val_loss:.2f}.hdf5'
+checkpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, verbose=1, save_best_only=True)
+
+callbacks =[
+    tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+    tf.keras.callbacks.TensorBoard(log_dir='logs'),
+    checkpoint]
+
+########## RESUME TRAINING ############
+latest_checkpoint = r'C:/Users/edwin.p.alegre/Google Drive/Synced Folders/Academics/Ryerson University - MASc/Courses/EE8204 - Neural Networks/Course Project/project_build_resunet/models/resunet.06-0.04.hdf5'
+
+model = tf.keras.models.load_model(latest_checkpoint)
+
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=batch_size, epochs=epochs, initial_epoch=6, callbacks=callbacks)
+
+
+########################################
+idx = random.randint(1, len(X_train))
+
+# pred_train = model.predict(X_train[:int(X_train.shape[0]*0.9)], verbose=1)
+# pred_val = model.predict(X_train[int(X_train.shape[0]*0.9):], verbose=1)
+pred_test = model.predict(X_test, verbose=1)
+
+# pred_train_mask = (pred_train > 0.5).astype(np.uint8)
+# pred_val_mask = (pred_val > 0.5).astype(np.uint8)
+pred_test_mask = (pred_test > 0.5).astype(np.uint8)
+
+# ix = random.randint(1, len(pred_train_mask))
+# fig = plt.figure()
+# fig.subplots_adjust(hspace=1, wspace=1)
+# ax = fig.add_subplot(2, 2, 1)
+# ax.imshow(X_train[ix])
+# ax = fig.add_subplot(2, 2, 2)
+# ax.imshow(np.squeeze(Y_train[ix]), cmap="gray")
+# ax = fig.add_subplot(2, 2, 3)
+# ax.imshow(np.squeeze(pred_train_mask[ix]), cmap="gray")
+
+
+# ix = random.randint(1, len(pred_val_mask))
+# fig = plt.figure()
+# fig.subplots_adjust(hspace=1, wspace=1)
+# ax = fig.add_subplot(2, 2, 1)
+# ax.imshow(X_train[int(X_train.shape[0]*0.9):][ix])
+# ax = fig.add_subplot(2, 2, 2)
+# ax.imshow(np.squeeze(Y_train[int(Y_train.shape[0]*0.9):][ix]), cmap="gray")
+# ax = fig.add_subplot(2, 2, 3)
+# ax.imshow(np.squeeze(pred_val_mask[ix]), cmap="gray")
+
+ix = random.randint(1, len(X_test))
+fig = plt.figure()
+fig.subplots_adjust(hspace=1, wspace=1)
+ax = fig.add_subplot(2, 2, 1)
+ax.imshow(X_test[ix])
+ax = fig.add_subplot(2, 2, 2)
+ax.imshow(np.squeeze(Y_test[ix]), cmap="gray")
+ax = fig.add_subplot(2, 2, 3)
+ax.imshow(np.squeeze(pred_test_mask[ix]), cmap="gray")
+
+imshow(np.squeeze(pred_test_mask[ix]), cmap="gray")
 
 # VERIFICATION 
 '''
@@ -56,14 +160,17 @@ ax = fig.add_subplot(1, 2, 1)
 ax.imshow(x[r])
 ax = fig.add_subplot(1, 2, 2)
 ax.imshow(np.reshape(y[r]*255, (image_size, image_size)), cmap="gray")
+
+'''
+
 '''
 
 # TRAINING
 train_gen = utils.DataGenerator(train_ids, train_dataset, batch_size=batch_size, image_size=image_size)
-test_gen = utils.DataGenerator(test_ids, test_dataset, batch_size=batch_size, image_size=image_size)
+validation_gen = utils.DataGenerator(validation_ids, train_dataset, batch_size=batch_size, image_size=image_size)
 
 train_step = len(train_ids)//batch_size
-test_step = len(test_ids)//batch_size
+validation_step = len(validation_ids)//batch_size
 
 epochs = 50
 
@@ -73,13 +180,20 @@ adam = Adam()
 model.compile(optimizer=adam, loss=utils.dice_coeff_loss, metrics=[utils.dice_coeff])
 model.summary()
 
-model.fit_generator(train_gen, validation_data=test_gen, steps_per_epoch=train_step, validation_steps=test_step, epochs=epochs)
+checkpoint = tf.keras.callbacks.ModelCheckpoint('roadseg.h5', verbose=1, save_best_only=True)
+
+callbacks =[
+    tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+    tf.keras.callbacks.TensorBoard(log_dir='logs')]
+
+
+model.fit_generator(train_gen, validation_data=validation_gen, steps_per_epoch=train_step, validation_steps=validation_step, epochs=epochs)
 
 print("\n      Ground Truth            Predicted Value")
 
-for i in range(1, testing_imgs + 1, 1):
+for i in range(1, validation_size + 1, 1):
     ## Dataset for prediction
-    x, y = test_gen.__getitem__(i)
+    x, y = validation_gen.__getitem__(i)
     result = model.predict(x)
     result = result > 0.4
     
@@ -92,5 +206,4 @@ for i in range(1, testing_imgs + 1, 1):
 
         ax = fig.add_subplot(1, 2, 2)
         ax.imshow(np.reshape(result[i]*255, (image_size, image_size)), cmap="gray")
-        
-        
+'''

utils.py
@@ -43,11 +43,13 @@ class DataGenerator(keras.utils.Sequence):
         
         # Read Image
         image = cv2.imread(image_path)
-        image = cv2.resize(image, (self.image_size, self.image_size))
+        if (image.shape[:-1] != (self.image_size, self.image_size)):
+            image = cv2.resize(image, (self.image_size, self.image_size))
         
         # Read Mask (as a greyscale)
         mask = cv2.imread(mask_path, 0)
-        mask = cv2.resize(mask, (self.image_size, self.image_size))
+        if (mask.shape != (self.image_size, self.image_size)):
+            mask = cv2.resize(mask, (self.image_size, self.image_size))
         mask = np.expand_dims(mask, axis=-1)
         
         # Normalize the image and mask